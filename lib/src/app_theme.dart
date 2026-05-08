0687792054bb676aec85ff7bcfa3349476656735
Josiah Troup <97867298+JoTroup@users.noreply.github.com>
2026-05-08 14:32:28 +0930

    Update app_theme.dart

--- lib/src/app_theme.dart
+++ lib/src/app_theme.dart
@@ -136,6 +136,7 @@ class AppTheme {
 
       textButtonTheme: TextButtonThemeData(
         style: ButtonStyle(
+          padding: WidgetStatePropertyAll(EdgeInsets.only(left: 8, right: 8, top: 2, bottom: 2)),
           splashFactory: InkRipple.splashFactory,
           backgroundColor: WidgetStatePropertyAll(Colors.transparent),
           foregroundColor: WidgetStatePropertyAll(_primaryColour)
