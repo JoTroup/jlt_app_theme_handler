be563f72a501ade4c1dccd1955813b50eb0bbb21
Josiah Troup <97867298+JoTroup@users.noreply.github.com>
2026-09-22 16:16:13 +0930

    Update app_theme.dart

--- lib/src/app_theme.dart
+++ lib/src/app_theme.dart
@@ -72,7 +72,7 @@ class AppTheme {
   }
 
   Color getPrimaryAccentColour() {
-    return TinyColor.fromColor(_primaryColour).spin(10).toColor();
+    return TinyColor.fromColor(_primaryColour).lighten(20).toColor();
   }
 
   getPrimaryBackgroundColour() {
